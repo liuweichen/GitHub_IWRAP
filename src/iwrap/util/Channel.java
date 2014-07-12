146d6f7c3edcc98ba38c8d1679ab74503c21561b
liuweichen <754075714@qq.com>
2014-07-12 20:13:45 +0800

    Second commit for aground

--- src/iwrap/util/Channel.java
+++ src/iwrap/util/Channel.java
@@ -3,9 +3,15 @@ package iwrap.util;
 public class Channel {
 	float length;//The length of the channel航道长度
 	float width;//The width of the channel航道宽度
+	float widthObstacle = 0f;//The width of the obstacle障碍物宽度
 	
+	public Channel(float length, float width, float widthObstacle) {
+		this.length = length;
+		this.width = width;
+		this.widthObstacle = widthObstacle;
+	}
+
 	public Channel(float length, float width) {
-		super();
 		this.length = length;
 		this.width = width;
 	}
@@ -22,11 +28,17 @@ public class Channel {
 	public void setWidth(float width) {
 		this.width = width;
 	}
+	public float getWidthObstacle() {
+		return widthObstacle;
+	}
+	public void setWidthObstacle(float widthObstacle) {
+		this.widthObstacle = widthObstacle;
+	}
 
-	
 	@Override
 	public String toString() {
-		return "Channel [length=" + length + ", width=" + width + "]";
+		return "Channel [length=" + length + ", width=" + width
+				+ ", widthObstacle=" + widthObstacle + "]";
 	}
 	
 }
